e5edffd4c713797726228b0b1a61b3b33dd4cc0d
azaelalanis <azaelalanis.g@gmail.com>
2014-11-23 20:43:03 -0600

    Se agregan todas las vistas y los paths en app.rb

--- app.rb
+++ app.rb
@@ -2,10 +2,40 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './config/environments' #database configuration
 
+set :views, settings.root + '/vistas'
+
 Dir['models/*.rb'].each do |model|
   require_relative model
 end
 
 get '/' do
-  "Hello, World!"
+  erb :login
+end
+
+get '/topics' do
+  erb :topicList
+end
+
+get '/topics/:topic/categories' do
+  erb :categoriesByTopic
+end
+
+get '/categories/:category/clues' do
+  erb :cluesByCategory
+end
+
+get '/dashboard' do
+  erb :dashboard
+end
+
+get '/profiles/:profile/edit' do
+  erb :editProfile
+end
+
+get '/students/new' do
+  erb :addStudents
+end
+
+get '/game/new' do
+  erb :newGameOptions
 end
